c406cc7b13fc1d2d89cde0c1664c77fe113ddef7
herp-a-derp <cat176@gmail.com>
2013-10-25 14:29:51 -0700

    I'm gradually working through the sorted diff of the decompiled source, copying over implementation details from the updates fenoxo has done in CoC to the codebase.

--- difProc/refactor.py
+++ difProc/refactor.py
@@ -7,7 +7,13 @@ import os.path
 
 files = ["CoC_0.8.2b1.swf_as-MainTimeline.as", "CoC_0.8.3c1.swf_as-MainTimeline.as"]
 
+# Find function declarations
+functionSearch = re.compile("[ 	]*?public (?=function [\w_]*?\([\w: ]*?\))")	# Woot! Positive lookahead! Because regexes aren't annoying enough already
+
+
 def sortFileContents():
+
+	doSort = True
 
 	for fileName in files:
 
@@ -15,17 +21,22 @@ def sortFileContents():
 		with open(fileName, "r") as fp:
 			contents = fp.read()
 
-			contentL = contents.split("function")		# WAAAAAAAYYYY not robust enough, currently does lots of messy stuff like split when there is a string containing 
-														# "function", in addition to actual function declarations.
-														# Dammit, I don't want to have to write a parser.
-			contentL.sort()
-			print len(contentL)
+			#print functionSearch.findall(contents)
+			contentL = functionSearch.split(contents)
+			
+			print "Found functions: ", len(contentL)
+
+			if doSort:
+				print "doing file sort"
+				
+				contentL.sort()
+				print len(contentL)
 
 
-			with open(fileName+".sort.as", "w") as fo:
-				for item in contentL:
-					fo.write("%s" % item.rstrip().lstrip())
-					fo.write("\n\\\\-----------------------------------------------------\n function ")
+				with open(fileName+".sort.as", "w") as fo:
+					for item in contentL:
+						fo.write("%s" % item.rstrip().lstrip())
+						fo.write("\n\\\\-----------------------------------------------------\n function ")
 
 if __name__ == "__main__":
 
