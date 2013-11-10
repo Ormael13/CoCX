4567b155f74ad2f669dba15b49d874f9e79c5fc4
Hoffledop <hoffledop@outlook.com>
2013-11-10 23:09:48 +0000

    Reworked the ImageManager system again, this time to make it work without all the hardships of loading in the xml.
    It is also fast, which was a nice bonus. New folder devTools to house the exe that builds the production xml that is packaged into the swf. Not intended to be released, it is only used to build the "master" xml file that includes all currently included images. It should be run whenever you made a new release. Also, it runs from devTools, don't move it because it doesn't have those nice features herp-derp included to protect you from yourself.

---
+++ devTools/AvailableImagesDict.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AvailableImagesDict
+{
+    class AvailableImagesDict
+    {
+        public static List<string> imageIDs = new List<string>();
+        public static List<string> fileName = new List<string>();
+
+        static void Main(string[] args)
+        {
+            DirSearch(@"../includes");
+            File.WriteAllLines(@"./allImages.txt", imageIDs);
+
+            BuildXMLFile();
+
+            Console.WriteLine("Complete. Press any key to exit.");
+            for (int i = 10; i > 0; i--)
+            {
+                Console.Write("\rAutomatically exiting in " + i + " seconds.");
+
+                for (int j = 20; j > 0; j--)
+                {
+                    // If the user has pressed a key, break
+                    if (Console.KeyAvailable)
+                        break;
+                    System.Threading.Thread.Sleep(50);
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        static void DirSearch(string sDir)
+        {
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir, "*.as", SearchOption.AllDirectories))
+                {
+                    foreach (string line in File.ReadAllLines(f))
+                    {
+                        foreach (Match match in Regex.Matches(line, "images\\.showImage\\(\"([^\"]*)\"\\)"))
+                        {
+                            Console.WriteLine("Found {0}", match.Groups[1].Value);
+                            imageIDs.Add(match.Groups[1].Value);
+                        }
+                    }
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        public static void BuildXMLFile()
+        {
+            try
+            {
+                Directory.CreateDirectory("../img/");
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.Indent = true;
+                settings.IndentChars = "\t";
+                XmlWriter writer = XmlWriter.Create("../img/images.xml", settings);
+                var filters = new String[] { "jpg", "jpeg", "png", "gif" };
+
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Images");
+
+                for (int i = 0; i < imageIDs.Count; i++)
+                {
+                    foreach (string fileType in filters)
+                    {
+                        writer.WriteStartElement("Image");
+                        writer.WriteAttributeString("id", imageIDs[i]);
+                        writer.WriteString("./img/" + imageIDs[i] + "." + fileType);
+                        writer.WriteEndElement();
+                    }
+                }
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+                writer.Close();
+            }
+            catch (Exception e)
+            {
+                
+            }
+        }
+    }
+}
