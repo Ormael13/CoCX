449503aac978f8711053887d9c4dc0d1ac039548
herp-a-derp <cat176@gmail.com>
2013-11-09 18:59:33 -0800

    More refactoring stuff

--- refactorTool.py
+++ refactorTool.py
@@ -370,6 +370,73 @@ def modifyParserIfStatements():
 
 
 
+def cleanAddButtons():
+
+	funcFind = re.compile(r"(if\(eventNo == \d*?\) )\{\W*")
+	funcExtr = re.compile(r"if\(eventNo == (\d*)\) (\w*)\(\)")
+
+	eventF = open("./includes/doEvent.as", "r")
+	eventS = eventF.read()
+	eventF.close()
+
+	eventS = funcFind.sub(r"\1", eventS)
+
+	print "done"
+	#print eventS
+	res = funcExtr.findall(eventS)
+	res = dict(res)
+	print res
+	#Ok, now we have a eventNo <-> function name dictionary
+	
+
+	buttonRE = re.compile(r"((?:addButton)|(?:anotherButton))\((\w+?),\s?(\"[\"\\A-Za-z0-9]+?\"),\s?eventParser,\s?(\d+?)\)")
+
+	searchREs = [buttonRE]
+
+	filelist = os.listdir("./includes")
+	print filelist
+	for filename in filelist:
+		if filename.endswith(".as") and not filename.find("doEvent")+1:			#Iterate over all the .as files in ./includes, and skip the doEvent file
+			#print filename
+			with open(os.path.join("./includes", filename), "r") as fileH:
+				tmp = fileH.read()
+
+			# BRUTE FORCE IT. BECAUSE LAZY
+			tmpO = tmp
+
+			for repRe in searchREs:
+				#print repRe.pattern
+				insertionPoint = 0
+				while repRe.search(tmp[insertionPoint:]):
+				
+					m = repRe.search(tmp[insertionPoint:])
+					start, length = m.start()+insertionPoint, len(m.group())
+
+					prefix, call, postfix = tmp[:start], tmp[start:start+length], tmp[start+length:]
+
+					oldCall = call
+					for key, value in res.iteritems():
+						if key in call:
+							#print key, value
+							call = call.replace(key, value)
+
+					if oldCall != call:		
+						print "Old:", oldCall
+						print "New:", call
+
+					tmp = prefix+call+postfix
+
+					insertionPoint = start+length
+			if len(tmp)-len(tmpO) != 0:
+				print len(tmpO), len(tmp)
+				print "Length Delta", len(tmp)-len(tmpO) 
+
+				#with open(os.path.join("./includes", filename), "w") as fileH:
+				#	tmp = fileH.write(tmp)
+
+
+
+
 if __name__ == "__main__":
 
 	print "OMG WE'S BREAKIN STUF!!111one!"
@@ -382,4 +449,6 @@ if __name__ == "__main__":
 	#insertFlags()
 	#functionYoink()
 
-	modifyParserIfStatements()
+	#modifyParserIfStatements()
+
+	cleanAddButtons()
