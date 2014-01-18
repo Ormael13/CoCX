Ormael13/CoCX

commit 0b64e1fbc3b1897787a55e5d7cca640662482aea

Monster.won method added instead of 50xx events. Magic numbers for these events replaced with function references.

classes/classes/Player.as
@@ -92,7 +92,7 @@ package classes
 				//Prevent negatives
 				if (HP<=0){
 					HP = 0;
-					if (game.gameState == 1 || game.gameState == 2) game.doNext(5010);
+					if (game.gameState == 1 || game.gameState == 2) game.doNext(game.endHpLoss);
 				}
 			}
 			return returnDamage;
@@ -933,7 +933,7 @@ package classes
 			//(HUGE – 2.4 - Size 12 + 5 Multi + 4 tits)
 			var total:Number = 0;
 			if (hasStatusAffect("Lactation Endurance") < 0)
-				createStatusAffect("Lactation Endurance", 1, 0, 0, 0);
+				createStatusAffect("Lactation EnNumbere", 1, 0, 0, 0);
 			total = biggestTitSize() * 10 * averageLactation() * statusAffectv1("Lactation Endurance") * totalBreasts();
 			if (statusAffectv1("Lactation Reduction") >= 48)
 				total = total * 1.5;

commit de9dca5a3f120124f7b9ddc780da21ef540d3495

Fixes https://github.com/herp-a-derp/Corruption-of-Champions/issues/269
But was introduced in 0b64e1fbc3b1897787a55e5d7cca640662482aea, when "Endurance" was replaced with "EnNumbere" somehow, in only one place.

classes/classes/Player.as
@@ -942,7 +942,7 @@ package classes
 			//(HUGE – 2.4 - Size 12 + 5 Multi + 4 tits)
 			var total:Number = 0;
 			if (hasStatusAffect("Lactation Endurance") < 0)
-				createStatusAffect("Lactation EnNumbere", 1, 0, 0, 0);
+				createStatusAffect("Lactation Endurance", 1, 0, 0, 0);
 			total = biggestTitSize() * 10 * averageLactation() * statusAffectv1("Lactation Endurance") * totalBreasts();
 			if (statusAffectv1("Lactation Reduction") >= 48)
 				total = total * 1.5;