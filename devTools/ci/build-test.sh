c3141db667ef1ce934d7a2c78b632c4e32b8a3f6
Brrr <brrritssocold@gmail.com>
2017-04-18 18:02:35 +0200

    Do not use sudo for setup

--- devTools/ci/build-test.sh
+++ devTools/ci/build-test.sh
@@ -3,4 +3,5 @@
 sudo apt-get install git -y
 git clone --depth=50 --branch=master https://github.com/Kitteh6660/Corruption-of-Champions-Mod.git coc
 cd coc
+echo "PATH is $PATH"
 ant debug -DFLEX_HOME="/usr/local/bin/apache-flex"
--- devTools/ci/setup-flex.sh
+++ devTools/ci/setup-flex.sh
@@ -10,12 +10,10 @@ USER_FRAMEWORK_DIR="$USER_LOCAL_DIR/lib"
 FLEX_DIR="$USER_BIN_DIR/apache-flex"
 FLEX_BIN_DIR="$FLEX_DIR/bin"
 
-sudo apt-get update
-sudo apt-get install ant -y
-
 wget -N "http://www-eu.apache.org/dist/flex/$VERSION/binaries/apache-flex-sdk-$VERSION-bin.tar.gz"
 tar zxf "apache-flex-sdk-$VERSION-bin.tar.gz"
-cd "apache-flex-sdk-$VERSION-bin"
+mv "apache-flex-sdk-$VERSION-bin" "flex"
+cd "flex"
 ant -f installer.xml -Dflash.donot.ask=true -Dair.donot.ask=true -Dfontswf.donot.ask=true
 
 echo "Creating $GLOBAL_PLAYER_DIR"
@@ -23,13 +21,3 @@ mkdir -vp $GLOBAL_PLAYER_DIR
 
 # TODO generate link with version number with sed or awk?
 wget -nc http://download.macromedia.com/get/flashplayer/updaters/23/playerglobal23_0.swc -O"$GLOBAL_PLAYER_PATH"
-
-echo "Creating $FLEX_DIR"
-sudo mkdir -p $FLEX_DIR
-
-echo "Copy files to $USER_LOCAL_DIR"
-cd ..
-sudo cp -r "apache-flex-sdk-$VERSION-bin"/* $FLEX_DIR
-sudo cp -r bin/* $USER_BIN_DIR
-sudo cp -r frameworks $USER_LOCAL_DIR
-sudo cp -r lib/* $USER_LIB_DIR
