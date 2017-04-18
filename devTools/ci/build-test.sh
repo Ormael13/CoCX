dbcf7c9b395f05d2f090c6d6199c42063a89094a
Brrr <brrritssocold@gmail.com>
2017-04-18 18:43:17 +0200

    Fixed build script for vagrant

--- devTools/ci/build-test.sh
+++ devTools/ci/build-test.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-sudo apt-get install git -y
-git clone --depth=50 --branch=master https://github.com/Kitteh6660/Corruption-of-Champions-Mod.git coc
-cd coc
-ant debug -DFLEX_HOME="/usr/local/bin/apache-flex"
+SRC_DIR='coc'
+
+sudo apt-get update
+sudo apt-get install git ant -y
+sh /vagrant/setup-flex.sh
+
+if [ ! -d "$SRC_DIR" ] ; then
+    git clone --depth=50 --branch=master https://github.com/Kitteh6660/Corruption-of-Champions-Mod.git "$SRC_DIR"
+fi
+
+cd $SRC_DIR
+
+git checkout origin/master
+
+ant debug -DFLEX_HOME="../flex"
