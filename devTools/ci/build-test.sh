b10fbcb06ffa5321fa691b134b3cbf626b2b4385
Brrr <brrritssocold@gmail.com>
2017-04-19 01:15:01 +0200

    Run flexunit tests on Travis CI

--- devTools/ci/build-test.sh
+++ devTools/ci/build-test.sh
@@ -3,8 +3,11 @@
 SRC_DIR='coc'
 
 sudo apt-get update
-sudo apt-get install git ant -y
+sudo apt-get install git ant vnc4server xfce4 -y
 sh /vagrant/setup-flex.sh
+
+Xvnc :1 &
+export DISPLAY=":1"
 
 if [ ! -d "$SRC_DIR" ] ; then
     git clone --depth=50 --branch=master https://github.com/Kitteh6660/Corruption-of-Champions-Mod.git "$SRC_DIR"
@@ -14,4 +17,4 @@ cd $SRC_DIR
 
 git checkout origin/master
 
-ant debug -DFLEX_HOME="../flex"
+ant test -DFLEX_HOME="../flex"
--- devTools/ci/setup-flex.sh
+++ devTools/ci/setup-flex.sh
@@ -6,7 +6,7 @@ GLOBAL_PLAYER_PATH="$GLOBAL_PLAYER_DIR/playerglobal.swc"
 
 wget -N "http://www-eu.apache.org/dist/flex/$VERSION/binaries/apache-flex-sdk-$VERSION-bin.tar.gz"
 tar zxf "apache-flex-sdk-$VERSION-bin.tar.gz"
-mv "apache-flex-sdk-$VERSION-bin" "flex"
+cp -r "apache-flex-sdk-$VERSION-bin" "flex"
 cd "flex"
 ant -f installer.xml -Dflash.donot.ask=true -Dair.donot.ask=true -Dfontswf.donot.ask=true
 
@@ -15,3 +15,10 @@ mkdir -vp $GLOBAL_PLAYER_DIR
 
 # TODO generate link with version number with sed or awk?
 wget -nc http://download.macromedia.com/get/flashplayer/updaters/23/playerglobal23_0.swc -O"$GLOBAL_PLAYER_PATH"
+
+# Setup flash player. You read that right! Flash player for linux is back!
+wget -N https://fpdownload.macromedia.com/pub/labs/flashruntimes/flashplayer/linux64/flash_player_sa_linux.x86_64.tar.gz
+tar zxf flash_player*
+
+# So flexunit can find the player and the ant build file does not have to be modified
+sudo cp flashplayer /usr/local/bin/gflashplayer
