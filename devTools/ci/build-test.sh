2bf256228caf4769aa70592c705ab31cc2c6e2af
Brrr <brrritssocold@gmail.com>
2017-04-18 18:11:49 +0200

    Set FLEX_HOME via .travis.yml

--- devTools/ci/build-test.sh
+++ devTools/ci/build-test.sh
@@ -3,5 +3,4 @@
 sudo apt-get install git -y
 git clone --depth=50 --branch=master https://github.com/Kitteh6660/Corruption-of-Champions-Mod.git coc
 cd coc
-echo "PATH is $PATH"
 ant debug -DFLEX_HOME="/usr/local/bin/apache-flex"
--- devTools/ci/setup-flex.sh
+++ devTools/ci/setup-flex.sh
@@ -3,12 +3,6 @@ VERSION='4.16.0'
 PLAYER_VERSION='23.0'
 GLOBAL_PLAYER_DIR="frameworks/libs/player/$PLAYER_VERSION"
 GLOBAL_PLAYER_PATH="$GLOBAL_PLAYER_DIR/playerglobal.swc"
-USER_LOCAL_DIR="/usr/local"
-USER_BIN_DIR="$USER_LOCAL_DIR/bin"
-USER_LIB_DIR="$USER_LOCAL_DIR/lib"
-USER_FRAMEWORK_DIR="$USER_LOCAL_DIR/lib"
-FLEX_DIR="$USER_BIN_DIR/apache-flex"
-FLEX_BIN_DIR="$FLEX_DIR/bin"
 
 wget -N "http://www-eu.apache.org/dist/flex/$VERSION/binaries/apache-flex-sdk-$VERSION-bin.tar.gz"
 tar zxf "apache-flex-sdk-$VERSION-bin.tar.gz"
