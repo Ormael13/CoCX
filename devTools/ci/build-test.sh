e0777a433ea65e50eec04a82958818a17805f58a
Brrr <brrritssocold@gmail.com>
2017-04-19 12:15:12 +0200

    Commented build files

--- devTools/ci/build-test.sh
+++ devTools/ci/build-test.sh
@@ -3,10 +3,16 @@
 SRC_DIR='coc'
 
 sudo apt-get update
+# install VNC and xfce4 so we have a desktop, as the flash player requires one
 sudo apt-get install git ant vnc4server xfce4 -y
+
+# download and install the flex SDK and the flash player
 sh /vagrant/setup-flex.sh
 
+# start a VNC session, this will be the desktop for tests
 Xvnc :1 &
+
+# this is to trick the flash player into thinking it is running on a desktop
 export DISPLAY=":1"
 
 if [ ! -d "$SRC_DIR" ] ; then
@@ -15,6 +21,8 @@ fi
 
 cd $SRC_DIR
 
+# checkout the master branch
 git checkout origin/master
 
+# build and run the tests
 ant test -DFLEX_HOME="../flex"
